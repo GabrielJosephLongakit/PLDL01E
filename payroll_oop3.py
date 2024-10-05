3bac37ba6c8c76b6217d98146692587a565f9826
GabrielJosephLongakit <2023-2-00285@lpunetwork.edu.ph>
2024-10-05 12:12:34 +0800

    push

--- payroll_oop3.py
+++ payroll_oop3.py
@@ -6,7 +6,7 @@ emp_num_of_absences = int(input("Enter value for number of absences:"))
 tardiness_hours = int(input("Enter number of tardiness:"))
 
 absences_deduction = employee_payroll.get_absences_deduction(emp_rate_per_hour, emp_num_of_absences)
-tardiness_hours = employee_payroll.get_tardiness_deduction(emp_rate_per_hour, tardiness_hours)
+tardiness_deduction = employee_payroll.get_tardiness_deduction(emp_rate_per_hour, tardiness_hours)
 
 partial_deduction = absences_deduction + tardiness_deduction
 print("Employee partial deduction is: %.2f" % partial_deduction)
